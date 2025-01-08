6e1c4deceace5ed3019792911384ccb5c9b439b1
azizovmuhammadali <alico.777777@gmail.com>
2025-01-08 14:23:44 +0500

    Added some part in uzbek

--- lang/uz/errors.php
+++ lang/uz/errors.php
@@ -1,6 +1,7 @@
 <?php
 return [
     'user' => [
+        'not_found'=> 'Foydalanuvchi topilmadi',
         'login'=> 'Sizning kiritgan malumotlaringizda hato bor',
         'verify'=> 'Siz email tasdiqlashingiz kerak',
     ],
