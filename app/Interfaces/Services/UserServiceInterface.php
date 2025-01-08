d501413dd7f23e2649adecbb95e0c6dd8d16f475
azizovmuhammadali <alico.777777@gmail.com>
2025-01-08 17:14:14 +0500

    Updated UserServiceInterface

--- app/Interfaces/Services/UserServiceInterface.php
+++ app/Interfaces/Services/UserServiceInterface.php
@@ -2,11 +2,9 @@
 
 namespace App\Interfaces\Services;
 
-use App\DTO\UserDTO;
-
 interface UserServiceInterface
 {
-  public function registerUser($userDTO);
-  public function loginUser($data);
-  public function verifyEmail($token);
+    public function registerUser($userDTO);
+    public function loginUser($data);
+    public function verifyEmail($token);
 }
