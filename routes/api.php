1b75b30209f9d8fe7f43c50bc4c7e11d75d4e053
azizovmuhammadali <alico.777777@gmail.com>
2025-01-08 09:47:17 +0500

    Added user routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::middleware('language')->group(function(){
+    Route::middleware('auth:sanctum')->group(function(){
+      Route::get('user',[AuthController::class,'findUser']);
+      Route::get('logout',[AuthController::class,'logout']);
+});
+Route::post('register',[AuthController::class,'register']);
+Route::post('login',[AuthController::class,'login']);
+Route::get('email-verify',[AuthController::class,'verifyEmail']);
+});
