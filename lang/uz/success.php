1df6b5705509cd8f8f38029fd7bffe2b867ec064
azizovmuhammadali <alico.777777@gmail.com>
2025-01-08 09:40:52 +0500

    Success responses in uzbek

---
+++ lang/uz/success.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'user' => [
+     'register' => 'Siz muvaffaqiyatli ro\'yxatdan o\'tdingiz',
+     'login'=> 'Siz muvaffaqiyatli kirdingiz',
+     'email'=> 'Email muvaffaqiyatli tasdiqlandi',
+     'logout'=> 'Muvaffaqiyatli chiqib ketildi',
+    ],
+];
