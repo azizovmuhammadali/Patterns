33c8ded63bbb886bf65168181e406ffa3b42fef3
azizovmuhammadali <alico.777777@gmail.com>
2025-01-08 09:39:40 +0500

    Advanced responses for users

---
+++ app/Traits/ResponseTrait.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Traits;
+
+trait ResponseTrait
+{
+    protected function success($data = [], string $message = 'Operation successful', int $status = 200)
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+            'data' => $data,
+        ], $status);
+    }
+    protected function responsePagination($paginator, $data = [], string $message = 'Operation successful', int $status = 200)
+    {
+        // Check if the first parameter is a paginator
+        if ($paginator instanceof \Illuminate\Pagination\LengthAwarePaginator) {
+            $pagination = [
+                'current_page' => $paginator->currentPage(),
+                'total_pages' => $paginator->lastPage(),
+                'total' => $paginator->total(),
+                'per_page' => $paginator->perPage(),
+                'links' => [
+                    'first' => $paginator->url(1),
+                    'last' => $paginator->url($paginator->lastPage()),
+                    'prev' => $paginator->previousPageUrl(),
+                    'next' => $paginator->nextPageUrl(),
+                ]
+            ];
+        } else {
+            $pagination = null; 
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+            'data' => $data,
+            'pagination' => $pagination, 
+        ], $status);
+    }
+    protected function error(string $message = 'An error occurred', int $status = 400)
+    {
+        return response()->json([
+            'status' => 'error',
+            'message' => $message,
+        ], $status);
+    }
+    public function uploadImage($image,$folderPath){
+        $filename = time() . '.' . $image->getClientOriginalExtension();
+        $uploadImage = $image->storeAs($folderPath,$filename,'public');
+        return $uploadImage;
+    }
+    public function deleteImage($path){
+        @unlink(storage_path('app/public/' . $path));
+    }
+    protected function prepareTranslations(array $translations, array $columns): array
+    {
+        $preparedTranslations = [];
+
+        foreach ($translations as $translation) {
+            foreach ($translation as $lang => $value) {
+                // Initialize the language key if not already present
+                if (!isset($preparedTranslations[$lang])) {
+                    $preparedTranslations[$lang] = [];
+                }
+
+                // Merge translation columns for the same language
+                foreach ($columns as $column) {
+                    if (isset($value[$column])) {
+                        $preparedTranslations[$lang][$column] = $value[$column];
+                    } else {
+                        info("{$column} not set for language: $lang");
+                    }
+                }
+            }
+        }
+
+        return $preparedTranslations;
+    }
+}
