d4b40bb3aace93ba811fa221e9dad4dccfaea136
azizovmuhammadali <alico.777777@gmail.com>
2025-01-10 09:54:07 +0500

    Updated

--- app/Interfaces/Services/BookServiceInterface.php
+++ app/Interfaces/Services/BookServiceInterface.php
@@ -4,5 +4,9 @@ namespace App\Interfaces\Services;
 
 interface BookServiceInterface
 {
-    //
+    public function getAllBook();
+    public function create($bookDTO);
+    public function getBookById($id);
+     public function update($id, $bookDTO);
+     public function delete($id);
 }
