43194830ad35a98aae054f50ccf88cbd2dbf6391
azizovmuhammadali <alico.777777@gmail.com>
2025-01-10 09:55:58 +0500

    Added function

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,12 @@
 <?php
 
-use App\Http\Middleware\SetLocaleMiddleware;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
+use App\Http\Middleware\SetLocaleMiddleware;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,6 +20,13 @@ return Application::configure(basePath: dirname(__DIR__))
         'language' => SetLocaleMiddleware::class,
        ]);
     })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
+   ->  withExceptions(function (Exceptions $exceptions) {
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if (request()->is('api/*') && $e->getPrevious() instanceof ModelNotFoundException) {
+                $model = Str::afterLast($e->getPrevious()->getModel(), '\\');
+                return response()->json(['message' => $model . ' not found'], 404);
+            }
+
+            return response()->json(['message' => __('errors.404')], 404);
+        });
     })->create();
