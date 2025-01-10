501f98c1fbcf0d9d8844373f34c89247d937dd0d
azizovmuhammadali <alico.777777@gmail.com>
2025-01-10 09:57:33 +0500

    Updated

--- app/Reposity/BookReposity.php
+++ app/Reposity/BookReposity.php
@@ -2,13 +2,41 @@
 
 namespace App\Reposity;
 
-class BookReposity
+use App\Models\Book;
+use App\Traits\ResponseTrait;
+use Illuminate\Support\Facades\Auth;
+use App\Interfaces\Reposity\BookReposityInterface;
+
+class BookReposity implements BookReposityInterface
 {
     /**
      * Create a new class instance.
      */
-    public function __construct()
-    {
-        //
-    }
+    use ResponseTrait;
+   public function index(){
+      $books = Book::with('author')->paginate(6);
+      return $books;
+   }
+   public function create($data){
+     $book = new Book();
+   $book->user_id = $data['user_id'];
+   $book->fill($data['translations']);
+   $book->save();
+   return $book->load('translations','author');
+   }
+   public function show($id){
+   $book = Book::findOrFail($id);
+   return $book->load('author','translations');
+   }
+   public function update($id, $data){
+  $book = Book::findOrFail($id);
+  $book->fill($data['translations']);
+  $book->save();
+  return $book->load('author','translations');
+   }
+   public function destroy($id){
+      $book = Book::findOrFail($id);
+    $book->delete();
+    return $this->success(__('success.books.deleted'));
+   }
 }
--- app/Services/BookService.php
+++ app/Services/BookService.php
@@ -1,14 +1,47 @@
 <?php
 
 namespace App\Services;
+use App\Traits\ResponseTrait;
+use App\Traits\TranslationTrait;
+use Illuminate\Support\Facades\Auth;
+use App\Interfaces\Services\BookServiceInterface;
+use App\Interfaces\Reposity\BookReposityInterface;
 
-class BookService
+class BookService implements BookServiceInterface
 {
     /**
      * Create a new class instance.
      */
-    public function __construct()
-    {
-        //
-    }
+    use TranslationTrait;
+    use ResponseTrait;
+    public function __construct(protected BookReposityInterface $bookReposityInterface){}
+  public function getAllBook()
+{
+    return $this->bookReposityInterface->index(); 
+}
+  public function create($bookDTO){
+    $translations = $this->prepareTranslations(['translations' => $bookDTO->translations], ['title','description']);
+        $data = [
+            'user_id' => Auth::id(),
+            'translations' => $translations
+        ];
+        return $this->bookReposityInterface->create($data);
+  }
+  public function getBookById($id){
+    return $this->bookReposityInterface->show($id);
+  }
+  public function update($id,$bookDTO){
+    if(Auth::id() !== $bookDTO->user_id){
+      return $this->error(__('errors.books.forbidden'), 403);
+  }
+  $translations = $this->prepareTranslations(['translations' => $bookDTO->translations], ['title','description']);
+  $data = [
+    'user_id' => $bookDTO->user_id,
+    'translations' => $translations,
+];
+     return $this->bookReposityInterface->update($id, $data);
+  }
+public function delete($id){
+  return $this->bookReposityInterface->destroy($id);
+}
 }
