4e6d8ab674f91d32c639a489eb0b62c7e1a3bf4e
azizovmuhammadali <alico.777777@gmail.com>
2025-01-10 09:54:00 +0500

    Updated

--- app/Interfaces/Reposity/BookReposityInterface.php
+++ app/Interfaces/Reposity/BookReposityInterface.php
@@ -4,5 +4,9 @@ namespace App\Interfaces\Reposity;
 
 interface BookReposityInterface
 {
-    //
+    public function index();
+    public function create($data);
+    public function show($id);
+    public function update($id, $data);
+    public function destroy($id);
 }
