2a942a82b73ad788f4cda9442532f6970bf10f06
azizovmuhammadali <alico.777777@gmail.com>
2025-01-10 09:53:49 +0500

    title and translation for book

--- app/Http/Resources/BookTranslationResource.php
+++ app/Http/Resources/BookTranslationResource.php
@@ -14,6 +14,10 @@ class BookTranslationResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'title' => $this->title,
+            'description' => $this->description,
+            'locale' => $this->locale,
+        ];
     }
 }
